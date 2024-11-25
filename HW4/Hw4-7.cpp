ac285e78270f67578ff0fd5d40ed3cc20f60f28b
Chanodom Onkulna <ochanodom@gmail.com>
2024-11-25 20:56:35 +0700

    import hw4-7

---
+++ HW4/Hw4-7.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int celdeg ;
+    float fahdeg ;
+    printf( "User input : " ) ;
+    scanf( "%d", &celdeg ) ;
+    fahdeg = ( celdeg * 9.0 / 5.0 ) + 32 ;
+    printf( "%d degree Celsius = %.1f degree Fahrenheit", celdeg, fahdeg ) ;
+    return  0 ;
+}//end function
