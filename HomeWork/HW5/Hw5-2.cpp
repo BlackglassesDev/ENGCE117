7da05fe29e866c3db9974e31697b6594e0aed059
Chanodom Onkulna <ochanodom@gmail.com>
2024-11-26 22:01:39 +0700

    edit comment

--- HomeWork/HW5/Hw5-2.cpp
+++ HomeWork/HW5/Hw5-2.cpp
@@ -10,21 +10,21 @@ int main() {
     printf( "Output: \n") ;
     if( num1 > num2 && num2 > num3) {
         printf( "%d %d %d", num1, num2, num3 ) ;
-    }
+    }//end if
     else if( num1 > num3 && num3 > num2 ) {
         printf( "%d %d %d", num1, num3, num2 ) ;
-    }
+    }//end else if
     else if( num2 > num1 && num1 > num3 ) {
         printf( "%d %d %d", num2, num1, num3 ) ;
-    }
+    }//end else if
     else if( num2 > num3 && num3 > num1 ) {
         printf( "%d %d %d", num2, num3, num1 ) ;
-    }
+    }//end else if
     else if( num3 > num1 && num1 > num2 ) {
         printf( "%d %d %d", num3, num1, num2 ) ;
-    }
+    }//end else if
     else {
         printf( "%d %d %d", num3, num2, num1 ) ;
-    }
+    }//end else
     return 0 ;
-}
+}//end function
