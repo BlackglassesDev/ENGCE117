ace4348b21e24254fe43243da24bdedc4286bbb7
Chanodom Onkulna <ochanodom@gmail.com>
2024-11-26 20:51:15 +0700

    update hw5-1

---
+++ HomeWork/HW5/Hw5-1.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    char ch1, ch2, ch3, ch4 ;
+    printf( "Test case: \n" ) ;
+    scanf( "%c %c %c %c", &ch1, &ch2, &ch3, &ch4 ) ;
+    printf( "Result: %c %c %c %c", ch4, ch3, ch2, ch1 ) ;
+}//end function
